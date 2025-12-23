36f96c37f950322257fbd299c6161179d4c8495a
vanbinh-star <nguyenvanbinhqn12345@gmail.com>
2025-12-23 23:19:01 +0700

    Add files via upload

---
+++ Models/ThongKe/tbXacNHanGioNCKH.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Models.QLKHCanBo;
+
+[Table("tbXacNHanGioNCKH")]
+public partial class tbXacNhanGioNCKH
+{
+    [Key]
+    public int IdXacNhanGioNCKH { get; set; }
+
+    public int? IdDonVi { get; set; }
+
+    public string? NamHoc { get; set; }
+
+    public int? IdCanBoXacNhan{ get; set; }
+
+    public string? FileTongHop { get; set; }
+
+    public DateOnly? NgayXacNhan { get; set; }
+
+}
