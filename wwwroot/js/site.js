5af0bee0c5963c9f95e3b65b7965740265fb6a37
vanbinh-star <nguyenvanbinhqn12345@gmail.com>
2025-12-25 00:08:16 +0700

    Update site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -9,3 +9,23 @@ window.scrollTo(0, parseInt(scrollPos, 10));
 sessionStorage.removeItem('scrollPos');
 }
 };
+window.downloadFileFromText = function (fileName, content) {
+    const blob = new Blob([content], { type: 'text/csv;charset=utf-8;' });
+
+    if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+        // Support for IE
+        window.navigator.msSaveOrOpenBlob(blob, fileName);
+        return;
+    }
+
+    const link = document.createElement('a');
+    const url = URL.createObjectURL(blob);
+
+    link.href = url;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+};
